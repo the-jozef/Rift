be64d418b6c164e4b2c933da9964abf037af987e
Jozef Lisko <jozeflisko0@gmail.com>
2026-03-25 21:29:38 +0100

    test02

--- Rift_App/MainWindow.xaml.cs
+++ Rift_App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Rift_App.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace Rift_App
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ Rift_App/ViewModels/Model_MVVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+
+namespace Rift_App.ViewModels
+{
+   
+    
+}
