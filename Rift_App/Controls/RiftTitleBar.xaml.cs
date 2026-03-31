868721ee08fa09ca185b42dd62dd23b10c9a645d
Jozef Lisko <jozeflisko0@gmail.com>
2026-03-31 22:44:04 +0200

    PopupBox_fix

--- Rift_App/Controls/RiftTitleBar.xaml.cs
+++ Rift_App/Controls/RiftTitleBar.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using MaterialDesignThemes.Wpf;
 
 namespace Rift_App.Controls
 {
