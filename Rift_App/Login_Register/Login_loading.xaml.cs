3f01c96ff713509ab07a0de8a03559535ab3aec3
Moskalm25 <Moskalm25@Z00T1-07>
2026-04-08 11:47:54 +0200

    1

---
+++ Rift_App/Login_Register/Login_loading.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Rift_App.ViewModels;
+
+namespace Rift_App.Login_Register
+{
+    /// <summary>
+    /// Interaction logic for Login_loading.xaml
+    /// </summary>
+    public partial class Login_loading : UserControl
+    {
+        public Login_loading()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+    }
+}
---
+++ Rift_App/ViewModels/ViewNavigator.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using Rift_App.Login_Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rift_App.ViewModels
+{
+    public partial class Navigator : ObservableObject
+    {
+        [ObservableProperty]
+        private object currentView;
+
+        public Navigator()
+        {
+            CurrentView = new Login();
+        }
+
+        [RelayCommand]
+        public void ShowLogin()
+        {
+            CurrentView = new Login();
+        }
+
+        [RelayCommand]
+        public void ShowRegister()
+        {
+            CurrentView = new Register(); 
+        }
+        [RelayCommand]
+        public void LoginLoading()
+        {
+            CurrentView = new Login_loading();
+        }
+    }
+}
