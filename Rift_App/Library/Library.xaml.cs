7a39b3ba6741d6b1699639312f6dc28366d7f901
Jozef Lisko <jozeflisko0@gmail.com>
2026-04-12 23:57:24 +0200

    library

--- Rift_App/Library/Library.xaml.cs
+++ Rift_App/Library/Library.xaml.cs
@@ -23,6 +23,48 @@ namespace Rift_App.Library
         public Library()
         {
             InitializeComponent();
+
+            for(int i = 1; i <= 60; i++)
+{
+                var button = new Button
+                {
+                    Background = Brushes.Transparent,
+                    BorderBrush = Brushes.Transparent,
+                    Height = 23
+                };
+
+                var stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    Width = 235,
+                    Height = 22
+                };
+
+                var image = new Image
+                {
+                    Height = 20,
+                    Width = 20,
+                    Margin = new Thickness(5, 0, 0, 0),
+                    Stretch = Stretch.UniformToFill,
+                    VerticalAlignment = VerticalAlignment.Bottom,
+                    Source = new BitmapImage(new Uri("/Icons/Red.png", UriKind.Relative))
+                };
+
+                var textBlock = new TextBlock
+                {
+                    Text = "Game " + i,
+                    Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#9197A5")),
+                    FontSize = 15,
+                    FontWeight = FontWeights.SemiBold,
+                    Margin = new Thickness(6,-2, 0, 0)
+                };
+
+                stackPanel.Children.Add(image);
+                stackPanel.Children.Add(textBlock);
+                button.Content = stackPanel;
+                MyStackPanel.Children.Add(button);
+            }
+
         }
     }
 }
